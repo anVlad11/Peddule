81f00c72f43d5d1621acafcc3d900a63733fbde5
anVlad11 <anVlad11@users.noreply.github.com>
2014-04-10 12:07:27 -0700

    Useful improvement: Working as vibrator now

--- src/peddule.c
+++ src/peddule.c
@@ -3,31 +3,40 @@
 Window *window;
 TextLayer *text_layer;
 
+static void second_tick(struct tm* tick_time, TimeUnits units_changed) { //время в формате HH:MM:SS
+	time_t now = time(NULL);
+	struct tm *current_time = localtime(&now);
+	static char time[] = "00:00:00"; 
+	strftime(time, sizeof(time), "%T", current_time);
+	text_layer_set_text(text_layer, time);
+	static const uint32_t const segments[] = { 995 };
+	VibePattern pat = {
+  	.durations = segments,
+  	.num_segments = ARRAY_LENGTH(segments),
+	};
+	vibes_enqueue_custom_pattern(pat);
+}
+
 void handle_init(void) {
 	// Create a window and text layer
 	window = window_create();
 	text_layer = text_layer_create(GRect(0, 0, 144, 154));
-	
+	window_set_background_color(window, GColorBlack);
 	// Set the text, font, and text alignment
-	text_layer_set_text(text_layer, "Generic text for future schedule watchapp");
+	tick_timer_service_subscribe(SECOND_UNIT, &second_tick);
+	text_layer_set_text_color(text_layer, GColorWhite);
+	text_layer_set_background_color(text_layer, GColorClear);
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);	
 	// Add the text layer to the window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
-
-	// Push the window
 	window_stack_push(window, true);
-	
-	// App Logging!
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
 
-void handle_deinit(void) {
-	// Destroy the text layer
+void handle_deinit(void) { //TODO: Не забывать вписывать сюда вообще всё
 	text_layer_destroy(text_layer);
-	
-	// Destroy the window
+	tick_timer_service_unsubscribe();
 	window_destroy(window);
 }
 
